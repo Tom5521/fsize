3e4e4919295cc1d3ab068acace52d7f56c4fa9db
Tom5521 <thomas.bored5521@gmail.com>
2024-04-18 22:58:23 +0000

    feat(main): Add info to flags

--- flags.go
+++ flags.go
@@ -3,6 +3,10 @@ package main
 import "flag"
 
 var (
-	PrintOnWalk = flag.Bool("print-on-walk", false, "")
-	NoWalk      = flag.Bool("no-walk", false, "")
+	PrintOnWalk = flag.Bool("print-on-walk", false,
+		"Prints the name of the file being walked if a directory has been selected.",
+	)
+	NoWalk = flag.Bool("no-walk", false,
+		"Skips walking inside the directories.",
+	)
 )
