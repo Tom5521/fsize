ba14a3eebf62ec31b90bb981d9db0d6151b4c3d0
Tom5521 <thomas.bored5521@gmail.com>
2024-07-12 16:19:55 -0400

    fix(stat): Fix missing function body on windows compilation

--- stat/ids_windows.go
+++ stat/ids_windows.go
@@ -8,8 +8,8 @@ import (
 	"os/user"
 )
 
-func UsrAndGroup(info os.FileInfo) (usr *user.User, group *user.Group, err error)
+func UsrAndGroup(info os.FileInfo) (usr *user.User, group *user.Group, err error) { return }
 
-func Usr(info os.FileInfo) (usr *user.User, err error)
+func Usr(info os.FileInfo) (usr *user.User, err error) { return }
 
-func Group(info os.FileInfo) (group *user.Group, err error)
+func Group(info os.FileInfo) (group *user.Group, err error) { return }
