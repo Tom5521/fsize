3e36e92468d70e52f0b23018bac16af018a114d8
Tom5521 <thomas.bored5521@gmail.com>
2025-10-08 01:12:49 -0300

    fix(stat): fix freebsd and netbsd compatibility

---
+++ stat/access_date_bsd.go
@@ -0,0 +1,18 @@
+//go:build darwin || freebsd || netbsd
+
+package stat
+
+import (
+	"os"
+	"syscall"
+	"time"
+)
+
+func AccessDate(info os.FileInfo) (t time.Time, err error) {
+	stat, ok := info.Sys().(*syscall.Stat_t)
+	if !ok {
+		return t, ErrGettingStruct
+	}
+	t = time.Unix(int64(stat.Atimespec.Sec), int64(stat.Atimespec.Nsec))
+	return t, err
+}
